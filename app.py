e1f755a778907ce738f361984a41544d69e22082
George Abban <102395479+Geobilly@users.noreply.github.com>
2024-07-27 14:58:46 +0000

    newn

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ from fetchdata import get_feeding_fees
 from fetchStudent import get_students
 from fetch_financial import fetch_financial
 from deletestudent import delete_student
+from FetchDeletedTransactions import get_deleted_transactions
+from DeleteTransaction import delete_transaction
+
+
+
+
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -23,6 +29,10 @@ app.route('/feeding_fees', methods=['GET'])(get_feeding_fees)
 app.route('/students', methods=['GET'])(get_students)
 app.route('/fetch-financial', methods=['POST'])(fetch_financial)
 app.route('/delete_student/<string:stu_id>', methods=['DELETE'])(delete_student)
+app.route('/deleted_transactions', methods=['GET'])(get_deleted_transactions)
+app.route('/delete_transaction', methods=['GET'])(delete_transaction)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
